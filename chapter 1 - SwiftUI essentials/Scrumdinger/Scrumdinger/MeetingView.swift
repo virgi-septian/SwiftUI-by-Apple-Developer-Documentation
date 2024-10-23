d822d07f3d16fa0242bfe991518436105ef35ab6
virgi-septian <virgiseptian77@gmail.com>
2024-10-23 16:46:39 +0700

    Modify and style views

--- chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/MeetingView.swift
+++ chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -15,13 +15,26 @@ struct MeetingView: View {
         HStack {
             VStack(alignment: .leading) {
                 Text("Second Elapsed")
+                    .font(.caption)
                 Label("300", systemImage: "hourglass.tophalf.fill")
             }
+            Spacer()
             VStack(alignment: .trailing) {
                 Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                    .font(.caption)
                 Label("600", systemImage: "hourglass.bottomhalf.fill")
             }
         }
+        Circle()
+            .strokeBorder(lineWidth: 24)
+        HStack{
+            Text("Speaker 1 of 3")
+            Spacer()
+            Button(action: {}) {
+                Image(systemName: "forward.fill")
+            }
+        }
+        .padding()
     }
 }
 
