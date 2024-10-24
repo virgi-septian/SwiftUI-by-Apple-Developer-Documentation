5a646bbb414634ca5cb38a11fe58e4ff2ed6fef6
virgi-septian <virgiseptian77@gmail.com>
2024-10-24 11:16:26 +0700

    Customize the label style

---
+++ chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,24 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Virgi Septian on 24/10/24.
+//
+//  The goal is to create a label with an icon placed to the right (trailing) of its text. By default, in SwiftUI, the icon on a Label is usually placed to the left of the text, but with this custom style, we can change that.
+
+import SwiftUI
+
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
