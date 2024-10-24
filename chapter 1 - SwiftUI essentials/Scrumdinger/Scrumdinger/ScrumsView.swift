7a61049a5cd3ef777b91a778f9abe5114f7bb0de
virgi-septian <virgiseptian77@gmail.com>
2024-10-24 15:33:41 +0700

    Make scrums identifiable

--- chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/ScrumsView.swift
+++ chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -12,13 +12,9 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums, id: \.title) { scrum in
-            
+        List(scrums) { scrum in
             CardView(scrum: scrum)
                 .listRowBackground(scrum.theme.mainColor)
-                .onAppear{
-                    print("Main color for \(scrum.title): \(scrum.theme.mainColor)")
-                }
         }
     }
 }
