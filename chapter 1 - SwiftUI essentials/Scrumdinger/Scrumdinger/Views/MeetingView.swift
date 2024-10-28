c63bcb9510e825acce7489bf31699e30d2678eb8
virgi-septian <virgiseptian77@gmail.com>
2024-10-28 16:44:37 +0700

    Trigger sound with AVFoundation

--- chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/Views/MeetingView.swift
+++ chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/Views/MeetingView.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
@@ -28,6 +32,10 @@ struct MeetingView: View {
         .onAppear {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
             scrumTimer.startScrum()
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
         }
         .onDisappear {
             scrumTimer.stopScrum()
