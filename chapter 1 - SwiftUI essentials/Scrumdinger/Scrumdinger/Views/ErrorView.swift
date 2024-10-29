72398991ee7e1ec0cd596ef50e685e66fe3db24f
virgi-septian <virgiseptian77@gmail.com>
2024-10-29 16:48:46 +0700

    Create an error view

---
+++ chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,44 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Virgi Septian on 29/10/24.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occured!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
+
