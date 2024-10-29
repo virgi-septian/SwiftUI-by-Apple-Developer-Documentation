4887edadf3308155de24f64dc621d747e7e89fea
virgi-septian <virgiseptian77@gmail.com>
2024-10-29 14:39:15 +0700

    Save data in inactive state

--- chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -12,14 +12,22 @@ struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $store.scrums)
-                .task {
+            ScrumsView(scrums: $store.scrums) {
+                Task {
                     do {
                         try await store.load()
                     } catch {
                         fatalError(error.localizedDescription)
                     }
                 }
+            }
+            .task {
+                do {
+                    try await store.load()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+            }
         }
     }
 }
--- chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/Views/ScrumsView.swift
+++ chapter 1 - SwiftUI essentials/Scrumdinger/Scrumdinger/Views/ScrumsView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
+    let saveAction: ()->Void
     
     var body: some View {
         NavigationStack {
@@ -33,12 +35,19 @@ struct ScrumsView: View {
         .sheet(isPresented: $isPresentingNewScrumView) {
             NewScrumSheet(scrums: $scrums, isPresentingNewScrum: $isPresentingNewScrumView)
         }
+//        .onChange(of: scenePhase) { newPhase in
+//            if newPhase == .inactive { saveAction() }
+//        }
+        .onChange(of: scenePhase) { oldPhase, newPhase in
+            if newPhase == .inactive { saveAction() }
+        }
+        
     }
 }
 
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: .constant(DailyScrum.sampleData) )
+        ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {} )
     }
 }
