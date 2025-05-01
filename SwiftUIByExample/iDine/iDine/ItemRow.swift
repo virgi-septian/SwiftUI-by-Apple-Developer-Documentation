de8418887a81ac7f03595eb976afe8222a248fe7
virgi-septian <virgiseptian77@gmail.com>
2025-05-01 20:57:46 +0700

    Polishing designs with fonts adn colors

--- SwiftUIByExample/iDine/iDine/ItemRow.swift
+++ SwiftUIByExample/iDine/iDine/ItemRow.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ItemRow: View {
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     let item: MenuItem
     var body: some View {
         HStack {
-            Image(item.thumbnailImage)
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }
+            Spacer()
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(2)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundStyle(.white)
             }
         }
     }
