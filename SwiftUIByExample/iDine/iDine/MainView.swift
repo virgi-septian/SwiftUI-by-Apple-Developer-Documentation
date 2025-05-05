c7c8a33a2ecf0d56515760b853c2ea787e6e79a9
virgi-septian <virgiseptian77@gmail.com>
2025-05-05 10:22:05 +0700

    Adding TabView and tabItem()

---
+++ SwiftUIByExample/iDine/iDine/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Virgi Septian on 05/05/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        .environmentObject(Order())
+}
--- SwiftUIByExample/iDine/iDine/iDineApp.swift
+++ SwiftUIByExample/iDine/iDine/iDineApp.swift
@@ -12,7 +12,7 @@ struct iDineApp: App {
     @State var order = Order()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environmentObject(order)
         }
     }
