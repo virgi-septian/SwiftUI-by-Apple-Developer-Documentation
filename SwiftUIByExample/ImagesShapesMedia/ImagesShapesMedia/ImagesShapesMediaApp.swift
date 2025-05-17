0156d4676a8b6637cf45ebeb209ae9abbdd9ecdf
virgi-septian <virgiseptian77@gmail.com>
2025-05-17 22:20:49 +0700

    DrawImagesUsingImageViews

---
+++ SwiftUIByExample/ImagesShapesMedia/ImagesShapesMedia/ImagesShapesMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesShapesMediaApp.swift
+//  ImagesShapesMedia
+//
+//  Created by Virgi Septian on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ImagesShapesMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DrawImagesUsingImageViews()
+        }
+    }
+}
