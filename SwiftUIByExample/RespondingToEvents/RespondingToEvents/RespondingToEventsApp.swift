5fd6b3ab8692ab3e09bbd277e71e41c4bcd5dc4d
virgi-septian <virgiseptian77@gmail.com>
2025-08-04 11:42:52 +0700

    add an AppDelegate to a SwiftUI app

--- SwiftUIByExample/RespondingToEvents/RespondingToEvents/RespondingToEventsApp.swift
+++ SwiftUIByExample/RespondingToEvents/RespondingToEvents/RespondingToEventsApp.swift
@@ -10,6 +10,16 @@ import SwiftUI
 @main
 struct RespondingToEventsApp: App {
     
+    /// SwiftUI apps launch using a custom struct that conforms to the App protocol, but sometimes you might want to get back the old UIApplicationDelegate functionality we used to have – perhaps to handle registration for push notifications, to respond to memory warnings, to detect time changes, and so on.
+//    class AppDelegate: NSObject, UIApplicationDelegate {
+//        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//            print("Your code here")
+//            return true
+//        }
+//    }
+//    
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     /// run code when the app launches
     init() {
         UserDefaults.standard.register(defaults: [
@@ -17,6 +27,8 @@ struct RespondingToEventsApp: App {
             "highScore": 10
         ])
     }
+    
+   
     
     var body: some Scene {
         WindowGroup {
