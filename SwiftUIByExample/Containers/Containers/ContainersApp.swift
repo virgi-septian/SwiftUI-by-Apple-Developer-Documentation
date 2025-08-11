bfb798f359c8754e58e42c13ba62f7c698ae6c0c
virgi-septian <virgiseptian77@gmail.com>
2025-08-11 09:40:13 +0700

    create scrolling pages of content using tabViewStyle()

--- SwiftUIByExample/Containers/Containers/ContainersApp.swift
+++ SwiftUIByExample/Containers/Containers/ContainersApp.swift
@@ -11,7 +11,22 @@ import SwiftUI
 struct ContainersApp: App {
     var body: some Scene {
         WindowGroup {
-            EmbedViewsInATabBar()
+//            EmbedViewsInATabBar()
+//            CreateScrollingPagesOfContent()
+            
+            /// your have to run the simulator to see the result
+//            TabView {
+//                Text("First")
+//                Text("Second")
+//                Text("Third")
+//                Text("Fourth")
+//            }
+//            .tabViewStyle(.page)
+            /// You can control the way paging dots are shown by adding an extra parameter to the .page method. For example, if you never wanted the paging dots to be shown, you would use this:
+//            .tabViewStyle(.page(indexDisplayMode: .never))
+//            .indexViewStyle(.page(backgroundDisplayMode: .always))
+            
+            
         }
     }
 }
