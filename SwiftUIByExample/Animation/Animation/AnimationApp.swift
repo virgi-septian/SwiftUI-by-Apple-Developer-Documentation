6a3562994ef5b9907165009ac5c8939ad820e207
virgi-septian <virgiseptian77@gmail.com>
2025-08-19 11:11:18 +0700

    create basic animations

---
+++ SwiftUIByExample/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Virgi Septian on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CreateBasicAnimations()
+        }
+    }
+}
---
+++ SwiftUIByExample/Animation/Animation/CreateBasicAnimations.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Virgi Septian on 19/08/25.
+//
+
+import SwiftUI
+
+struct CreateBasicAnimations: View {
+    @State private var scaleEffect = 0.0
+    @State private var angel = 0.0
+    @State private var borderThickness = 1.0
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .scaleEffect(scaleEffect)
+                .animation(.bouncy(duration: 1), value: scaleEffect)
+            
+            Button("Gedein") {
+                scaleEffect += 1
+            }
+            
+            Button("Kecilin") {
+                scaleEffect -= 1
+            }
+        }
+        .padding()
+        
+        
+        /// You can animate many other modifiers, such as 2D and 3D rotation, opacity, border, and more. For example, this makes a button that spins around and increases its border every time it’s tapped:
+        Button("Click") {
+            angel += 45
+            borderThickness += 1
+        }
+        .padding()
+        .border(.red, width: borderThickness)
+        .rotationEffect(Angle(degrees: angel))
+    }
+}
+
+#Preview {
+    CreateBasicAnimations()
+}
