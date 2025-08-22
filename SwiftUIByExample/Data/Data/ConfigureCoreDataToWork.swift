8b09ead3716f9fe53dd3868121d9e7db47592c5e
virgi-septian <virgiseptian77@gmail.com>
2025-08-22 09:25:10 +0700

    configure Core Data to work with SwiftUI

---
+++ SwiftUIByExample/Data/Data/ConfigureCoreDataToWork.swift
@@ -0,0 +1,18 @@
+//
+//  ConfigureCoreDataToWork.swift
+//  Data
+//
+//  Created by Virgi Septian on 22/08/25.
+//
+
+import SwiftUI
+
+struct ConfigureCoreDataToWork: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    ConfigureCoreDataToWork()
+}
---
+++ SwiftUIByExample/Data/Data/DataApp.swift
@@ -0,0 +1,23 @@
+//
+//  DataApp.swift
+//  Data
+//
+//  Created by Virgi Septian on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct DataApp: App {
+    let persistenceController = PersistenceController.shared
+    @Environment(\.scenePhase) var scenePhase
+    var body: some Scene {
+        WindowGroup {
+            ConfigureCoreDataToWork()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: scenePhase) { _ in
+            persistenceController.save()
+        }
+    }
+}
