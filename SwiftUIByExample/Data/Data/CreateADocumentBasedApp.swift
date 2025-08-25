13cd77233b8e224c47d4a65b5c484e254a447a2f
virgi-septian <virgiseptian77@gmail.com>
2025-08-25 13:53:26 +0700

    export files using fileExporter()

--- SwiftUIByExample/Data/Data/CreateADocumentBasedApp.swift
+++ SwiftUIByExample/Data/Data/CreateADocumentBasedApp.swift
@@ -8,6 +8,34 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
+//struct TextFile: FileDocument {
+//    // tell the system we support only plain text
+//    static var readableContentTypes = [UTType.plainText]
+//
+//    // by default our document is empty
+//    var text = ""
+//
+//    // a simple initializer that creates new, empty documents
+//    init(initialText: String = "") {
+//        text = initialText
+//    }
+//
+//    // this initializer loads data that has been saved previously
+//    init(configuration: ReadConfiguration) throws {
+//        if let data = configuration.file.regularFileContents {
+//            text = String(decoding: data, as: UTF8.self)
+//        } else {
+//            throw CocoaError(.fileReadCorruptFile)
+//        }
+//    }
+//
+//    // this will be called when the system wants to write our data to disk
+//    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+//        let data = Data(text.utf8)
+//        return FileWrapper(regularFileWithContents: data)
+//    }
+//}
+
 struct TextFile: FileDocument {
     // tell the system we support only plain text
     static var readableContentTypes = [UTType.plainText]
@@ -37,9 +65,30 @@ struct TextFile: FileDocument {
 }
 
 struct CreateADocumentBasedApp: View {
+    @State private var showingExporter = false
     @Binding var document: TextFile
     var body: some View {
-        TextEditor(text: $document.text)
+        VStack {
+            TextEditor(text: $document.text)
+                .padding()
+        }
+        .toolbar {
+            Button("Export") {
+                showingExporter = true
+            }
+        }
+        .fileExporter(
+            isPresented: $showingExporter,
+            document: document,
+            contentType: .plainText
+        ) { result in
+            switch result {
+            case .success(let url):
+                print("Saved to \(url)")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
